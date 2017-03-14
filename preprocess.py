6d2e5b97fac543c37b4a89e257f139f19c2db431
Avraam <atolmid@gmail.com>
2017-03-14 13:15:35 +0100

    Add data augmentation. Process training and validation data pickle separately, so some memory can be freed from the already processed data

--- preprocess.py
+++ preprocess.py
@@ -4,6 +4,7 @@ import numpy as np
 import os
 import csv
 #import pickle
+# using zodbpickle, since normal pickle has an issue with large files
 from zodbpickle import pickle
 
 # There is 1 output class
@@ -37,9 +38,69 @@ print("Validation Data Shape: {}".format(np.array(validation_samples).shape))
 print()
 
 batch_size=100
+correction = 0.25
 
 
-    
+def random_shear(image, angle, shear_dist=50):
+    '''
+    Randomly shear image.
+    '''
+    rows, cols, _ = image.shape
+    d = np.random.randint(-shear_dist, shear_dist+1)
+    pt_1 = np.float32([[0, rows], [cols, rows], [cols/2, rows/2]])
+    pt_2 = np.float32([[0, rows], [cols, rows], [cols/2+d, rows/2]])
+    dsteer = d / (rows/2) * .4875
+    M = cv2.getAffineTransform(pt_1, pt_2)
+    image = cv2.warpAffine(image, M, (cols,rows), borderMode=1)
+    angle += dsteer
+    return image, angle
+
+
+def random_rotate(image, angle, angle_range=5):
+    '''
+    Randomly rotate image.
+    '''
+    dangle = np.random.uniform(-angle_range, angle_range)
+    rows, cols, _ = image.shape
+    M = cv2.getRotationMatrix2D((cols/2,rows/2),dangle,1)
+    image = cv2.warpAffine(image, M, (cols,rows), borderMode=1)
+    dsteer = - dangle * np.pi / 180 * .4875
+    angle += dsteer
+    return image, angle
+
+def augment_brightness_camera_images(image):
+    image1 = cv2.cvtColor(image,cv2.COLOR_RGB2HSV)
+    image1 = np.array(image1, dtype = np.float64)
+    random_bright = .5+np.random.uniform()
+    image1[:,:,2] = image1[:,:,2]*random_bright
+    image1[:,:,2][image1[:,:,2]>255]  = 255
+    image1 = np.array(image1, dtype = np.uint8)
+    image1 = cv2.cvtColor(image1,cv2.COLOR_HSV2RGB)
+    return image1
+
+def add_random_shadow(image):
+    top_y = 320*np.random.uniform()
+    top_x = 0
+    bot_x = 160
+    bot_y = 320*np.random.uniform()
+    image_hls = cv2.cvtColor(image,cv2.COLOR_RGB2HLS)
+    shadow_mask = 0*image_hls[:,:,1]
+    X_m = np.mgrid[0:image.shape[0],0:image.shape[1]][0]
+    Y_m = np.mgrid[0:image.shape[0],0:image.shape[1]][1]
+    shadow_mask[((X_m-top_x)*(bot_y-top_y) -(bot_x - top_x)*(Y_m-top_y) >=0)]=1
+    #random_bright = .25+.7*np.random.uniform()
+    if np.random.randint(2)==1:
+        random_bright = .5
+        cond1 = shadow_mask==1
+        cond0 = shadow_mask==0
+        if np.random.randint(2)==1:
+            image_hls[:,:,1][cond1] = image_hls[:,:,1][cond1]*random_bright
+        else:
+            image_hls[:,:,1][cond0] = image_hls[:,:,1][cond0]*random_bright    
+    image = cv2.cvtColor(image_hls,cv2.COLOR_HLS2RGB)
+    return image
+
+
 def load_data(samples):
     images = []
     angles = []
@@ -57,12 +118,12 @@ def load_data(samples):
             #print("name1 : ", name1)
             left_image = cv2.imread(name1)
             images.append(left_image)
-            angles.append(center_angle+0.1)
+            angles.append(center_angle+correction)
             name2 = './data/IMG/'+batch_sample[2].split('/')[-1]
             #print("name1 : ", name2)
             right_image = cv2.imread(name2)
             images.append(right_image)
-            angles.append(center_angle-0.1)
+            angles.append(center_angle-correction)
         except ValueError: 
             print('Line {} is corrupt!'.format(i))
     print("images number : ", len(images))
@@ -70,35 +131,6 @@ def load_data(samples):
     return images, angles
     
 
-#==============================================================================
-# def load_data(samples):
-#     images = []
-#     angles = []
-#     print("samples length : ", len(samples))
-#     i = 0
-#     for offset in range(0, len(samples), batch_size):
-#         batch_samples = samples[offset:offset+batch_size]
-#         print("offset : ", offset)
-#         try:
-#             i+=1
-#             for batch_sample in batch_samples:
-#                 name = './data/IMG/'+batch_sample[0].split('/')[-1]
-#                 center_image = cv2.imread(name)
-#                 center_angle = float(batch_sample[3])
-#                 images.append(center_image)
-#                 angles.append(center_angle)
-#         except ValueError: 
-#             print('Line {} is corrupt!'.format(offset+i))
-#     for batch_sample in samples[offset: len(samples)]:
-#             name = './data/IMG/'+batch_sample[0].split('/')[-1]
-#             center_image = cv2.imread(name)
-#             center_angle = float(batch_sample[3])
-#             images.append(center_image)
-#             angles.append(center_angle)
-#     print("images number : ", len(images))
-#     print("angles number : ", len(angles))
-#     return images, angles
-#==============================================================================
 
 def flip_data(images, angles):
     augmented_images, augmented_angles  = [], []
@@ -111,15 +143,50 @@ def flip_data(images, angles):
     print("augmented_angles number : ", len(augmented_angles))
     return augmented_images, augmented_angles
 
+def augmentation(images, angles):
+    augmented_images, augmented_angles  = [], []
+    for image, angle in zip(images, angles):
+        augmented_images.append(image)
+        augmented_angles.append(angle)
+        augmented_images.append(add_random_shadow(image))
+        augmented_angles.append(angle)
+        augmented_images.append(augment_brightness_camera_images(image))
+        augmented_angles.append(angle)
+        image_tr, angle_tr = random_rotate(image, angle, angle_range=5)
+        augmented_images.append(image_tr)
+        augmented_angles.append(angle_tr)
+        image_tr, angle_tr = random_shear(image, angle, shear_dist=50)
+        augmented_images.append(image_tr)
+        augmented_angles.append(angle_tr)
+    print("augmented images number : ", len(augmented_images))
+    print("augmented labels number : ", len(augmented_angles))
+    return augmented_images, augmented_angles
+
+
 
 train_images, train_angles = load_data(train_samples)
+# print the dataset shape
+print()
+print("Training Data Shape: {}".format(np.array(train_images).shape))
+print()
+
 train_images, train_angles = flip_data(train_images, train_angles)
-validation_images, validation_angles = load_data(validation_samples)
-validation_images, validation_angles = flip_data(validation_images, validation_angles)
+# print the dataset shape
+print()
+print("Training Data Shape: {}".format(np.array(train_images).shape))
+print()
+
+
+train_images, train_angles = augmentation(train_images, train_angles)
+# print the dataset shape
+print()
+print("Training Data Shape: {}".format(np.array(train_images).shape))
+print()
 
 train = {'train_dataset': train_images, 'train_labels': train_angles}
-validation  = {'valid_dataset': validation_images, 'valid_labels': validation_angles}
 
+del train_images
+del train_angles
 
 ## Save the data for later access
 train_file = 'train.pickle'
@@ -132,17 +199,6 @@ while not stop:
         try:
             with open(train_file, 'wb') as f:
               pickle.dump(train, f, pickle.HIGHEST_PROTOCOL)
-#==============================================================================
-#             with open(pickle_file, 'wb') as pfile:
-#                 pickle.dump(
-#                     {
-#                         'train_dataset': train_images,
-#                         'train_labels': train_angles,
-#                         'valid_dataset': validation_images,
-#                         'valid_labels': validation_angles
-#                     },
-#                     pfile, pickle.HIGHEST_PROTOCOL)
-#==============================================================================
         except Exception as e:
             print('Unable to save data to', train_file, ':', e)
             raise
@@ -153,22 +209,47 @@ while not stop:
         print("Please use a different file name")
         pickle_file = input("Enter: ")
         
+
+del train
+
+validation_images, validation_angles = load_data(validation_samples)
+# print the dataset shape
+
+print()
+print("Validation Data Shape: {}".format(np.array(validation_images).shape))
+print()
+
+validation_images, validation_angles = flip_data(validation_images, validation_angles)
+# print the dataset shape
+
+print()
+print("Validation Data Shape: {}".format(np.array(validation_images).shape))
+print()
+
+validation_images, validation_angles = augmentation(validation_images, validation_angles)
+# print the dataset shape
+print()
+print("Validation Data Shape: {}".format(np.array(validation_images).shape))
+print()
+
+
+
+validation  = {'valid_dataset': validation_images, 'valid_labels': validation_angles}
+
+del validation_images
+del validation_angles
+
+## Save the data for later access
+train_file = 'train.pickle'
+validation_file ='validation.pickle'
+stop = False
+
+while not stop:      
     if not os.path.isfile(validation_file):
         print('Saving data to pickle file...')
         try:
             with open(validation_file, 'wb') as f:
               pickle.dump(validation, f, pickle.HIGHEST_PROTOCOL)
-#==============================================================================
-#             with open(pickle_file, 'wb') as pfile:
-#                 pickle.dump(
-#                     {
-#                         'train_dataset': train_images,
-#                         'train_labels': train_angles,
-#                         'valid_dataset': validation_images,
-#                         'valid_labels': validation_angles
-#                     },
-#                     pfile, pickle.HIGHEST_PROTOCOL)
-#==============================================================================
         except Exception as e:
             print('Unable to save data to', validation_file, ':', e)
             raise
@@ -178,3 +259,6 @@ while not stop:
     else:
         print("Please use a different file name")
         pickle_file = input("Enter: ")
+        
+
+del validation
